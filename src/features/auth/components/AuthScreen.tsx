a36e779cc3c904d174acc39d927f991a0954e04e
seyaendris <seide9894@gmail.com>
2024-08-27 21:49:40 +0300

    updated the AuthScreen

--- src/features/auth/components/AuthScreen.tsx
+++ src/features/auth/components/AuthScreen.tsx
@@ -11,7 +11,7 @@ const AuhtScreen = () => {
   return (
     <div className='h-full flex items-center justify-center bg-[#5c3b58]'>
       <div className='md:h-auto md:w-[420px]'>
-          {state === "signIn" ? <SigninCard /> : <SignupCard />}
+          {state === "signIn" ? <SigninCard setState = {setState} /> : <SignupCard setState = {setState}  />}
       </div>
     </div>
   )
