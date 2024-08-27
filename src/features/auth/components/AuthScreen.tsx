18330b4b3967db19682fea4261a6dd94014e5de4
seyaendris <seide9894@gmail.com>
2024-08-27 20:57:02 +0300

    created auth-screen components

---
+++ src/features/auth/components/AuthScreen.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+
+import React, { useState } from 'react'
+import { SigninFlow } from '../types'
+import SigninCard from './SigninCard'
+import SignupCard from './SignupCard'
+
+const AuhtScreen = () => {
+  const [state, setState] = useState<SigninFlow>("signIn")
+  return (
+    <div className='h-full flex items-center justify-center bg-[#5c3b58]'>
+      <div className='md:h-auto md:w-[420px]'>
+          {state === "signIn" ? <SigninCard /> : <SignupCard />}
+      </div>
+    </div>
+  )
+}
+
+export default AuhtScreen
---
+++ src/features/auth/types.tsx
@@ -0,0 +1,1 @@
+export type SigninFlow = "signIn" | "signUp"
