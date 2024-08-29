f18c52331d073b98007cf12b3550eb420709ed10
seyaendris <seide9894@gmail.com>
2024-08-29 21:21:38 +0300

    created users query to get the current user

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import AuhtScreen from '../../features/auth/components/AuthScreen';
+
+const AuthPage = () => {
+  return (
+   <AuhtScreen />
+  )
+}
+
+export default AuthPage
