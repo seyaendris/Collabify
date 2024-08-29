6d58e03b5acc6bf4ad40b823478247caba547881
seyaendris <seide9894@gmail.com>
2024-08-29 21:35:11 +0300

    updated the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import AuhtScreen from "@/features/auth/components/AuthScreen";
+"use client"
 
+import { Authenticated, Unauthenticated, AuthLoading } from "convex/react";
+import AuhtScreen from '@/features/auth/components/AuthScreen';
+
+import { SignOut } from "@/features/auth/components/SignOut";
+import UserButton from "@/features/auth/components/UserButton";
 
 export default function Home() {
+
   return (
-  <>
+    <>
+    <AuthLoading>{<p>Loading...</p>}</AuthLoading>
+    <Unauthenticated>
       <AuhtScreen />
-  </>    
+    </Unauthenticated>
+    <Authenticated>
+      Logged In
+      <UserButton />
+      {/* <SignOut /> */}
+      
+    </Authenticated>
+  </>   
   );
 }
