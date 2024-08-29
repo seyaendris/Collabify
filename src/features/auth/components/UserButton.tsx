21e4ebb783a1cd0900ba00f29e380a5b8161c7f6
seyaendris <seide9894@gmail.com>
2024-08-29 21:30:30 +0300

    created userbutton component

---
+++ src/features/auth/components/UserButton.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
+import React from 'react'
+
+const UserButton = () => {
+  return (
+   <DropdownMenu modal={false}>
+        <DropdownMenuTrigger className='outline-none relative'>
+            <Avatar className='size-10 hover:opacity-75 transition'>
+                <AvatarImage />
+                <AvatarFallback>
+
+                </AvatarFallback>
+            </Avatar>
+
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent align='center' side='right' className='w-60'>
+            <DropdownMenuItem>
+                
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+   </DropdownMenu>
+  )
+}
+
+export default UserButton
