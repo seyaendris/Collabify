257b7025f8a2d8784cb682317f8bc4a2896fa9e5
seyaendris <seide9894@gmail.com>
2024-08-31 22:56:32 +0300

    updated workspaces.ts

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -1,4 +1,34 @@
-import { query } from "./_generated/server";
+import { v } from "convex/values";
+import { query, mutation } from "./_generated/server";
+import { auth } from "./auth";
+import { getAuthUserId } from "@convex-dev/auth/server";
+
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await auth.getUserId(ctx)
+
+    if (!userId) {
+      throw new Error('Unauthorized')
+    }
+
+    const joinCode = '123456'
+
+    const workspaceId = await ctx.db.insert('workspaces', {
+      name: args.name,
+      userId,
+      joinCode
+    })
+
+    // const workspace = await ctx.db.get(workspaceId)
+
+    return workspaceId
+
+  }
+})
 
 export const get = query({
   args: {},
