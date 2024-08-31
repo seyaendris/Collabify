ea81bb5bd9df010c6d80f678fffb3b044b37ca9f
seyaendris <seide9894@gmail.com>
2024-08-31 23:01:17 +0300

    created a workspace modal

---
+++ src/components/Modals.tsx
@@ -0,0 +1,12 @@
+import CreateWorkspaceModal from '@/features/workspaces/components/CreateWorkspaceModal'
+import React from 'react'
+
+const Modals = () => {
+  return (
+    <>
+        <CreateWorkspaceModal />
+    </>
+  )
+}
+
+export default Modals
